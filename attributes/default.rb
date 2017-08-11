dd804b4903166064933307460db5a4ac0d18fb8c
Caleb Severn <calsev@gmail.com>
2017-08-11 15:53:12 -0700

    Fixed quotes around variable name

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,11 +3,11 @@
 tcb = 'win_firewall'
 
 # Attribute to provide a list of protected groups that will not be deleted
-default['tcb']['firewall_to_whitelist_groups'] = {
+default[tcb]['firewall_to_whitelist_groups'] = {
   'default' => ['core networking']
 }
 
 # Attribute to provide a list of protected rules that will not be deleted
-default['tcb']['firewall_to_whitelist_rules'] = {
+default[tcb]['firewall_to_whitelist_rules'] = {
   'default' => []
 }
