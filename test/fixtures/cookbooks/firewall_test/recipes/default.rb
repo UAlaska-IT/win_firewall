35fce1e5637a1da9f22a1fc3f56b6d0bd3d3b48b
calsev <calsev@gmail.com>
2018-07-30 13:37:30 -0800

    Added test for deleting ipv6 rules

--- test/fixtures/cookbooks/firewall_test/recipes/default.rb
+++ test/fixtures/cookbooks/firewall_test/recipes/default.rb
@@ -4,6 +4,13 @@ firewall 'default' do
   action :create
 end
 
+['dhcpv6', 'icmpv6', 'iphttps', 'ipv6', 'teredo'].each do |name_regex|
+  firewall_rule_state name_regex do
+    action :delete
+    use_regex true
+  end
+end
+
 firewall 'default' do
   action :log_scripts
 end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+describe powershell('netsh advfirewall firewall show rule name=all verbose') do
+  [/dhcpv6/i, /icmpv6/i, /iphttps/i, /ipv6/i, /teredo/i].each do |name_regex|
+    its(:stdout) { should_not match(name_regex) }
+  end
+end
+
 
 describe file('C:/chef/log/win_firewall.log') do
   it { should exist }
