65781a3376d5a8dc7f56f787bdf4016fcd7f54b2
Caleb Severn <calsev@gmail.com>
2018-08-02 23:25:31 -0800

    Updated chef client in kitchen

--- test/smoke/default/firewall_test.rb
+++ test/smoke/default/firewall_test.rb
@@ -12,7 +12,7 @@ end
 # WinRM over HTTP
 describe port(5985) do
   it { should be_listening }
-  its('protocols') { should include 'tcp' }
+  its(:protocols) { should include 'tcp' }
 end
 
 describe powershell('netsh advfirewall show currentprofile firewallpolicy | findstr "Firewall Policy"') do
